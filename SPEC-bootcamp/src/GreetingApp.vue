db2574b8a0b90ae30f3c3b763f2d3d16e12a47b6
Shambel Dechu <shambeldechu@gmail.com>
2026-02-04 23:49:21 +0300

    feat(day-2): add greetings vue app example

---
+++ SPEC-bootcamp/src/GreetingApp.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+import { ref } from 'vue';
+const name = ref('');
+</script>
+
+<template>
+    <div>
+        <input v-model="name" placeholder="Type your name">
+        <p>Selam, {{ name }}!</p>
+    </div>
+</template>
