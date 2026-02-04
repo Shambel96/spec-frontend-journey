56dec09314cdc7405935aef0a3c4827b8f8a758f
Shambel Dechu <shambeldechu@gmail.com>
2026-02-04 23:51:13 +0300

    feat: import greeting app into main

--- SPEC-bootcamp/src/main.js
+++ SPEC-bootcamp/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import GreetingApp from './greetingApp.vue'
 
+// Mount the main App component
 createApp(App).mount('#app')
+
+createApp(GreetingApp).mount('#app')
