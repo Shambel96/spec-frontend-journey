05c0d20e3db6582474764b10c35198ceb5203c8b
Shambel Dechu <shambeldechu@gmail.com>
2026-02-05 18:08:15 +0300

    feat: add userCard components

--- SPEC-bootcamp/src/App.vue
+++ SPEC-bootcamp/src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  <UserCard />
+  <div class="container">
+    <UserCard name="Mr.Z" role="Team Members" />
+    <UserCard name="Mr.X" role="Project Manager" />
+    <UserCard name="Mr.Y" role="Admin" />
+  </div>
 </template>
 
-<script>
-import UserCard from "./components/UserCard.vue";
-import HelloWorld from "./components/HelloWorld.vue";
-export default {
-  components: {
-    UserCard
-  }
-}
+<script setup>
+import UserCard from "./components/UserCard.vue"
 </script>
+
+<style>
+.container {
+  display: flex;
+  gap: 20px;
+  padding: 20px;
+}
+</style>
