2d3e1901b87d49285b12aa1e95182f77cf85fdd1
Shambel Dechu <shambeldechu@gmail.com>
2026-02-05 21:33:22 +0300

    feat: add user status display either online or not

--- SPEC-bootcamp/src/App.vue
+++ SPEC-bootcamp/src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="container">
-    <UserCard name="Mr.Z" role="Team Members" />
-    <UserCard name="Mr.X" role="Project Manager" />
-    <UserCard name="Mr.Y" role="Admin" />
+    <UserCard name="Mr.Z" role="Team Members"   status="online"
+      @view-profile="handleViewProfile"/>
+    <UserCard name="Mr.X" role="Project Manager"   status="offline"
+      @view-profile="handleViewProfile" />
+    <UserCard name="Mr.Y" role="Admin"   status="online"
+      @view-profile="handleViewProfile"/>
   </div>
 </template>
 
 <script setup>
 import UserCard from "./components/UserCard.vue"
+
+function handleViewProfile(name) {
+  alert(`Viewing profile of ${name}`)
+}
 </script>
 
 <style>
