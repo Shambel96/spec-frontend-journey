5e3abeaa23ed61216e0a9549f8bfd9eede21a2fb
Shambel Dechu <shambeldechu@gmail.com>
2026-02-05 17:15:12 +0300

    feat: add userCard pratcice example

---
+++ SPEC-bootcamp/src/components/UserCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card">
+    <h2>{{ name }}</h2>
+    <p>Role: {{ role }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserCard",
+  data() {
+    return {
+      name: "Shambel",
+      role: "SWE",
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  border: 1px solid #ccc;
+  padding: 16px;
+  border-radius: 8px;
+  width: 250px;
+}
+</style>
