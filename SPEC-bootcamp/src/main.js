5b0a7662022d6e917bc3839df9871b379e7c35cd
Shambel Dechu <shambeldechu@gmail.com>
2026-02-06 20:05:42 +0300

    import into main counter app

--- SPEC-bootcamp/src/main.js
+++ SPEC-bootcamp/src/main.js
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import GreetingApp from './greetingApp.vue'
-
+import Button from './components/button.vue'
+import Counter from './components/Counter.vue'
 // Mount the main App component
 createApp(App).mount('#app')
 
-createApp(GreetingApp).mount('#app')
+//createApp(GreetingApp).mount('#app')
+
+createApp(Button).mount('#button')
