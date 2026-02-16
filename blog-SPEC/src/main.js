7bb879e156ac13ed0050a793bbc781fb32ab5b3b
Shambel Dechu <shambeldechu@gmail.com>
2026-02-16 22:46:07 +0300

    feat: add content

--- blog-SPEC/src/main.js
+++ blog-SPEC/src/main.js
@@ -1,10 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
 import router from './router'
+import './style.css'
+
 
 createApp(App)
   .use(router)
