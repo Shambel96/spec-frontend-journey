d47b83d4019e5330ca5897f13d57df710dcc5995
Shambel Dechu <shambeldechu@gmail.com>
2026-02-18 21:38:53 +0300

    feat: add navbar and postcard components

---
+++ Blog-Platform/src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/create">Create Post</RouterLink>
+
+    <button @click="toggleAuth">
+      {{ isAuth ? 'Logout' : 'Login' }}
+    </button>
+  </nav>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const isAuth = ref(false)
+
+onMounted(() => {
+  isAuth.value = !!localStorage.getItem('isAuth')
+})
+
+function toggleAuth() {
+  if (isAuth.value) {
+    localStorage.removeItem('isAuth')
+  } else {
+    localStorage.setItem('isAuth', 'true')
+  }
+
+  isAuth.value = !isAuth.value
+}
+</script>
